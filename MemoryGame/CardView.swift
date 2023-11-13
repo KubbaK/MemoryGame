47ba3937927fc8b6fdffb1e0ca4e7a141f03b6bd
Jakub Kryk <65350295+KubbaK@users.noreply.github.com>
2023-11-14 00:30:39 +0100

    Update CardView.swift

--- MemoryGame/CardView.swift
+++ MemoryGame/CardView.swift
@@ -45,3 +45,43 @@ struct CardView: View {
 #Preview {
     CardView(karta: "😁")
 }
+
+
+
+
+
+
+
+// import SwiftUI
+
+// struct CardView: View {
+//     @State var isFaceUp: Bool = false
+//     var content: String
+//     var themeColor: Color
+    
+//     var body: some View {
+//         ZStack {
+//             Group {
+//                 RoundedRectangle(cornerRadius: 12)
+//                     .fill(.white)
+//                     .strokeBorder(themeColor, lineWidth: 2)
+                    
+//                 Text(content)
+//                     .font(.largeTitle)
+//             }
+//             .opacity(isFaceUp ? 1 : 0)
+//             RoundedRectangle(cornerRadius: 12)
+//                 .fill(themeColor)
+//                 .opacity(isFaceUp ? 0 : 1)
+//         }
+//         .onTapGesture (perform: {
+//             isFaceUp.toggle()
+//         })
+//     }
+// }
+
+// struct CardView_Previews: PreviewProvider {
+//     static var previews: some View {
+//         CardView(content: "😄", themeColor: Color.red)
+//     }
+// }
