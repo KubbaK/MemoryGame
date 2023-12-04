2569a121c2280a948cfe55fe6876d34914c72ccb
Jakub Kryk <65350295+KubbaK@users.noreply.github.com>
2023-12-05 00:29:02 +0100

    Update MemoryGameModel.swift

--- MemoryGame/MemoryGameModel.swift
+++ MemoryGame/MemoryGameModel.swift
@@ -43,55 +43,80 @@ struct MemoGameModel<CardContent : Equatable> {
 
 
 
-//
-//import Foundation
-//
-//struct MemoGameModel where CardContent : Equatable {
-//
-//    private(set) var cards: Array
-//    init(numberOfPairsOfCards: Int, cardContentFactory: (Int) -> CardContent) {
-//        cards=[]
-//        for pair in 0.. let content = cardContentFactory(pair)
-//                cards.append(Card(id:"\(pair+1)a", content: content))
-//                cards.append(Card(id:"\(pair+1)b",content: content))
-//    }
-//}
-//
-//
-//mutating func choose(_ card: Card) {
-//    if let chosenIndex = cards.firstIndex(where: {$0.id == card.id}) {
-//
-//        cards[chosenIndex].isFaceUp = true
-//
-//    }
-//}
-//
-//private func index(of card: Card) -> Int? {
-//    for index in cards.indices {
-//        if cards[index].id == card.id {
-//            return index
-//        }
-//    }
-//    return nil
-//}
-//
-//mutating func shuffle(){
-//    cards.shuffle()
-//}
-//
-//
-//struct Card : Equatable, Identifiable {
-//
-//    var id: String
-//    var isFaceUp: Bool = false
-//    var isMatched: Bool = false
-//    var content: CardContent
-//}
-//
-//}
-//
-//extension Array {
-//    var only: Element? {
-//        count == 1 ? first :nil
-//    }
-//}
+// import Foundation
+
+// struct MemoGameModel<CardContent> where CardContent : Equatable {
+//     private (set) var karty : Array<Karta>
+    
+//     init(numberPairsOfCard: Int, cardContentFactory: (Int)->CardContent){
+//         karty =  []
+        
+//         for pairIndex in 0..<max(2,numberPairsOfCard) {
+//             let content = cardContentFactory(pairIndex)
+//             karty.append(Karta(content: content, id: "\(pairIndex)a"))
+//             karty.append(Karta(content: content, id: "\(pairIndex)b"))
+//         }
+//     }
+    
+//     private var indexOfOneAndOnlyFaceUpCard: Int? {
+//             get {
+//                 let faceUpCardIndices = karty.indices.filter { karty[$0].isFaceUp && !karty[$0].isMatched}
+//                 return faceUpCardIndices.count == 1 ? faceUpCardIndices.first : nil
+//             }
+//             set {
+//                 for index in karty.indices {
+//                     if(!karty[index].isMatched){
+//                         karty[index].isFaceUp = (index == newValue)
+//                     }
+//                 }
+//             }
+//         }
+    
+//     func index(of karta: Karta) -> Int {
+//         return karty.firstIndex(where: {$0.id == karta.id})!
+//     }
+    
+//     mutating func shuffle() {
+//         karty.shuffle()
+//     }
+    
+//     mutating func choose(_ karta: Karta) {
+//         let chosenIndex = index(of: karta)
+        
+
+//         guard !karty[chosenIndex].isFaceUp, !karty[chosenIndex].isMatched else {
+//             return
+//         }
+        
+//         if let potentialMatchIndex = indexOfOneAndOnlyFaceUpCard {
+     
+//             if karty[chosenIndex].content == karty[potentialMatchIndex].content {
+//                 karty[chosenIndex].isMatched = true
+//                 karty[potentialMatchIndex].isMatched = true
+//                 print("Takie same")
+//             }
+
+//             print("Nadal odwrocone")
+//             karty[chosenIndex].isFaceUp = true
+    
+            
+            
+//         } else {
+//             print("Inne")
+//             indexOfOneAndOnlyFaceUpCard = chosenIndex
+//         }
+//     }
+
+    
+    
+    
+//     struct Karta: Equatable, Identifiable{
+       
+        
+//         var isFaceUp = false
+//         var isMatched = false
+//         var content: CardContent
+        
+//         var id: String
+//     }
+// }
