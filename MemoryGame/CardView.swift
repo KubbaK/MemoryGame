9dfa025b5332e71bcd1cfbf63ea84a19f503728e
Jakub Kryk <65350295+KubbaK@users.noreply.github.com>
2023-12-05 11:12:59 +0100

    Update CardView.swift

--- MemoryGame/CardView.swift
+++ MemoryGame/CardView.swift
@@ -73,3 +73,70 @@ struct CardView: View {
 //     }
 
 
+
+
+
+
+// import SwiftUI
+
+// struct TransformIntoCard: ViewModifier {
+//     let isFaceUp: Bool
+
+//        func body(content: Content) -> some View {
+//         let rectangle = RoundedRectangle(cornerRadius: 12)
+//            ZStack {
+//                rectangle.strokeBorder(lineWidth: 3)
+//                    .background(rectangle.fill(Color.white))
+//                    .overlay(content)
+//                rectangle.fill()
+//                    .opacity(isFaceUp ? 0 : 1)
+//            }
+//            .rotation3DEffect(
+//             .degrees(isFaceUp ? 0 : 180),
+//             axis: (0,1,0)
+//            )
+        
+//        }
+// }
+//    extension View {
+//        func transformIntoCard(isFaceUp: Bool) -> some View {
+//            modifier(TransformIntoCard(isFaceUp: isFaceUp))
+//        }
+//    }
+
+
+
+
+// import SwiftUI
+// import CoreGraphics
+
+// struct CirclePart: Shape {
+//     var startAngle: Angle = .zero
+//     let endAngle: Angle
+//     var clockwise = true
+    
+//     func path(in rect: CGRect) -> Path {
+//         let startAngle = startAngle - .degrees(90)
+//         let endAngle = endAngle - .degrees(90)
+        
+//         let center = CGPoint(x: rect.midX, y: rect.midY)
+//         let radius = min(rect.width, rect.height) * 0.5
+//         let start = CGPoint(
+//             x: center.x + radius * cos(CGFloat(startAngle.radians)),
+//             y: center.y + radius * sin(CGFloat(startAngle.radians))
+//         )
+        
+//         var path = Path()
+//         path.move(to: center)
+//         path.addLine(to: start)
+//         path.addArc(
+//             center: center,
+//             radius: radius,
+//             startAngle: startAngle,
+//             endAngle: endAngle,
+//             clockwise: !clockwise
+//         )
+//         path.addLine(to: center)
+//         return path
+//     }
+// }
